4a27e3c5d51f63e122a52d89c630ad92fb7f72e9
Abdulrahman-Al-Hasbani-UCLL <Abdulrahman.alhasbani@student.ucll.be>
2024-10-24 11:32:27 +0200

    Update API URL in .env file and add axios dependency

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
 import { Text, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Index() {
+  const API_URL = process.env.API_URL_Backend;
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (API_URL) {
+      axios.get(API_URL)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    } else {
+      console.error("API_URL is not defined");
+    }
+  }, []);
+
   return (
     <View className="flex justify-center items-center ">
-      <Text >Hello, this is the main page for the Restaurant manager x app</Text>
+      <Text>Hello, this is the main page for the Restaurant manager x app</Text>
     </View>
   );
 }
